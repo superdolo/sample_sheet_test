4c29a9294f6a76578c7d679f93b6416e932f9745
Steve Dolatowski <sdolatowski@chartertsp.com>
2017-09-08 12:35:52 -0500

    Final Commit. Validation Checks in place to ensure structure of xlsx is acceptable. All objectives met.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # sample_sheet_test
 Tempus Python Developer Applicant Test - Steve Dolatowski
+
+Installation:
+1. unpack sample_sheet_test.py and Sample_Sheet_Test.xlxs into folder
+
+Required Packages: 
+pandas numpy matplotlib xlrd
+
+Run: 
+from cmd line # python sample_sheet_test.py
+
+
+Notes: 
+
+The purpose of this projet was to demonstrate my ability with python and the pandas datascience package. 
+Each of the requirements should be met without issue. 
+
+I thoroughly enjoyed learning/using many of the features of the pandas package.
+In retrospect I would do a couple things differently:
+	1.	Execute the initial parse of the xlsx into series rather than directly into dataframe. 
+		Parsing everything into a dataframe to start cauese the rest of sorting to be a little tricky. 
+		I could have read the file multiple times and parsed different dataframes each time but I wanted to limit reading the file to once.
+	2.	Revisit my method for stripping and formating the data in the initial dataframe. 
+		The way it is currently done converts all fields to str type which works but could be a problem with a more diverse dataset
+		
+Thank You:
+
+Thanks to everyone at Tempus for taking the time and giving me the opertunity to show you how I can be an excelent member of your team.
+I had a great time with the challenge and am happy to add Pandas and Numpy to my toolbox for future projects.
+		
--- sample_sheet_test.py
+++ sample_sheet_test.py
@@ -5,7 +5,7 @@
 
 Open Sample_Sheet_Test.xlsx, using Pandas preferably
 
-1. Write a class for the parsing of the sheet. Assume the sheet is written by hand, and could contain errors. Raise errors if the sheet does not look like the one you are given, or, in other words, force the general structure of the sheet as given. Please be flexible and handle if there are spaces around names or capitalizations anywhere.
+x1. Write a class for the parsing of the sheet. Assume the sheet is written by hand, and could contain errors. Raise errors if the sheet does not look like the one you are given, or, in other words, force the general structure of the sheet as given. Please be flexible and handle if there are spaces around names or capitalizations anywhere.
 
 x2. Get the Experiment Name, save it to the class object. Make the assumption the formatting will stay the same, with a Date followed by "_"
 
@@ -35,22 +35,28 @@ import sys
 import datetime
 import re
 
-
 file_name = 'Sample_Sheet_Test.xlsx'
 
 class Sample:
 
 	section_names = ['[header]','[reads]','[settings]','[data]'] #just a little descriptive data we will use later to parse datasets to each section
-
-	def parse_sample(self,file):
+	header_headers = ['iemfileversion','investigator name','experiment name','date','workflow','application','assay','description','chemistry','nan'] #expected headers categories for validation/structure enforcement
+	data_headers  = ['lane','sample_id','sample_name','sample_plate','sample_well','i7_index_id','index','sample_project','description'] #expected headers categories for validation/structure enforcement
+	settings_headers = ['adapter','adapterread2','nan']#expected headers categories for validation/structure enforcement
+	valid_headers = {'header':header_headers,'data':data_headers,'settings': settings_headers} #dictionary of expected headers categories for validation/structure enforcement
+	
+	def parse_sample(self,file): #in retrospect i would parse as series for greater flexibility in creating dataframes later but this still works 
 		dirty_df = pd.read_excel(file, sheetname=0, header=None,index_col = None, na_values=['NA']) #If header rows are assumed to be consistent, this process can be made much simpler with header=[0, 1, 2, 3, etc]
-		clean_df = dirty_df.apply(lambda x: x.astype(str).str.strip().str.lower())
+		clean_df = dirty_df.apply(lambda x: x.astype(str).str.strip().str.lower()) #1. strip whitespace and convert to lower to decrease the change of errors due to inconsistencies in data entry
 		return clean_df
+	
 	def __init__(self,file_name):
 		xlsx = self.parse_sample(file_name) #Here we parse the entire xlsx
-		print(xlsx)
-		def generate_dataset(section_name): #write a function to find the starting positions of each section of data section_names = ['[Header]','[Reads]','[Settings]','[Data]']
-			row_id = int(xlsx.loc[xlsx[0] == section_name].index[0])
+		def generate_dataset(section_name): #generates a dataframe for each section in test xlsx
+			try:
+				row_id = int(xlsx.loc[xlsx[0] == section_name].index[0]) #find the starting positions of each section of data section_names = ['[Header]','[Reads]','[Settings]','[Data]']
+			except: 
+				raise ValueError(section_name + ' section was not found in sample!')
 			other_sections = [name for name in self.section_names if name != section_name]
 			def find_next_row_id(): #allows dataframes to adjust to data in different order by parsing dataframe to next section header instead of fixed by row# / df.index
 				try:
@@ -81,13 +87,35 @@ class Sample:
 		self.reads = generate_dataset('[reads]')
 		self.settings = generate_dataset('[settings]')
 		self.df = generate_dataset('[data]')
-
+		
+		def validate_structure(): #since my parsing is flexible to take the sections out of order and with extra lines I am validating by matching expected headers 
+			dataframes = [[self.header,'header'],[self.settings,'settings'],[self.df,'data']]
+			missing_headers = []
+			invalid_headers = []
+			for each in dataframes:
+				for hdr in list(each[0].columns.values):
+					if hdr not in self.valid_headers[each[1]]:
+						invalid_headers.append(hdr)
+				for hdr in self.valid_headers[each[1]]:
+					if hdr not in list(each[0].columns.values):
+						missing_headers.append(hdr)
+			if missing_headers != [] and invalid_headers == []:
+				raise ValueError('! Test is invalid ! The following headers were missing from the test: '+ ' '.join(missing_headers))
+			elif missing_headers == [] and invalid_headers != []:
+				raise ValueError('! Test is invalid ! The following invalid headers were found in the test: '+ ' '.join(invalid_headers))
+			elif missing_headers != [] and invalid_headers != []:
+				raise ValueError('! Test is invalid ! The following headers were missing from the test: '+ ' '.join(missing_headers) + ' and ' + 'The following invalid headers were found in the test: '+ ' '.join(invalid_headers))
+			else:
+				print('File Passed Validation Checks')
+				pass
+		
+		validate_structure() #runs the structure validation to ensure there are no bad data 
 		self.experiment_name = self.header['experiment name'].values[0]
 		self.experiment_date = self.header['date'].values[0]
 
-		print self.header
-		print self.experiment_name
-		print self.experiment_date
+		#print self.header
+		#print self.experiment_name
+		#print self.experiment_date
 
 		re = r'(?<=\A)\d{2}[a-z]{3}\d+_\d+[g|t]\b'
 
@@ -97,6 +125,7 @@ class Sample:
 		self.non_conforming = self.df[self.df.sample_id.str.contains('res') == True]
 		self.non_conforming = self.non_conforming[self.non_conforming.sample_id.str.contains(re)==False]
 		self.data = self.df[self.df.sample_id.str.contains(re)]
+		
 		self.sample_ids = {}
 		for id in self.df['sample_id'].values:
 			try:
@@ -107,25 +136,19 @@ class Sample:
 				self.sample_ids[main_id].append(id)
 			except:
 				self.sample_ids[main_id] = [id]
-	def validate_dates(self):
+	
+	def validate_dates(self): # method to check if the date matches the experiment_name
 		if datetime.datetime.strptime(self.experiment_name[:8],'%Y%m%d') == datetime.datetime.strptime(self.experiment_date,'%Y-%m-%d %H:%M:%S'):
 			print('Experiment Date: '+str(self.experiment_date)+' matches Experiment Name: '+ self.experiment_name)
 			return True
 		else:
-			print('! Experiment Date: '+str(self.experiment_date)+' DOES NOT MATCH Experiment Name: '+ self.experiment_name)
-			#raise ValueError('! Experiment Date: '+str(self.experiment_date)+' DOES NOT MATCH Experiment Name: '+ self.experiment_name)('Experiment Date: '+str(self.experiment_date)+' matches Experiment Name: '+ self.experiment_name)('! Experiment date does not match experiment name')
+			raise ValueError('! Experiment Date: '+str(self.experiment_date)+' DOES NOT MATCH Experiment Name: '+ self.experiment_name)
 
-test_sample = Sample(file_name)
-
-"""
-print (test_sample.header)
-print (test_sample.reads)
-print (test_sample.settings)
-print (test_sample.data)
-"""
+test_sample = Sample(file_name) # call class to create new Sample object
 
 print('Experiment Name: ' + test_sample.experiment_name)
 test_sample.validate_dates()
+
 
 print(test_sample.calibration_df)
 print(test_sample.non_conforming)
